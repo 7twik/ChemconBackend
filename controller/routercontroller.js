e4dcee4e45c9a6ac465e1571517fd0396876ec92
7twik <sattwik.das.ece25@heritageit.edu.in>
2023-12-23 17:06:12 +0530

    updated add

--- controller/routercontroller.js
+++ controller/routercontroller.js
@@ -111,39 +111,46 @@ exports.Bulk = async (req, res, next) => {
   exports.Add = async (req, res, next) => {
     const  email = req.body.data.email;
     const attendee= await AttendeeSchema.findOne({ Email:email  });
+    const chem= await ChemConMember.findOne({ Email:email  });
     try{
-      if(attendee){
-        res.json({ user:attendee, message: 'Already registered.',status:0 });
+      if(chem)
+      {
+        if(attendee){
+          res.json({ user:attendee, message: 'Already registered.',status:0 });
+        }
+        else{
+          const user = await ChemConMember.findOne({ Email:email  });
+          const name= user.Name; 
+          const mobile= user.Mobile; 
+          const url= req.body.data.qr; 
+          const category= user.Category; 
+          const kit= user.Kit; 
+          const verify= "Pre Registered";
+          const checkin1= false; 
+          const checkin2= false; 
+          const checkin3= false; 
+          
+          const newUser = await AttendeeSchema.create({ 
+              Name: name, 
+              Email: email, 
+              Mobile: mobile, 
+              Kit: kit, 
+              Url: url, 
+              VerifyCode: verify,
+              Category: category, 
+              checkin1: checkin1, 
+              checkin2: checkin2, 
+              checkin3: checkin3, 
+          }); 
+          console.log(newUser);
+          //sendOTPViaEmail(req.body.data.email, req.body.data.qr,name);
+          console.log("after email");
+          await res.json({user:newUser, message: "Email QR sent successfully"}); 
+          console.log("after email");// "success 
+        }
       }
       else{
-        const user = await ChemConMember.findOne({ Email:email  });
-        const name= user.Name; 
-        const mobile= user.Mobile; 
-        const url= req.body.data.qr; 
-        const category= user.Category; 
-        const kit= user.Kit; 
-        const verify= "Pre Registered";
-        const checkin1= false; 
-        const checkin2= false; 
-        const checkin3= false; 
-        
-        const newUser = await AttendeeSchema.create({ 
-            Name: name, 
-            Email: email, 
-            Mobile: mobile, 
-            Kit: kit, 
-            Url: url, 
-            VerifyCode: verify,
-            Category: category, 
-            checkin1: checkin1, 
-            checkin2: checkin2, 
-            checkin3: checkin3, 
-        }); 
-        console.log(newUser);
-        //sendOTPViaEmail(req.body.data.email, req.body.data.qr,name);
-        console.log("after email");
-        await res.json({user:newUser, message: "Email QR sent successfully"}); 
-        console.log("after email");// "success 
+        res.json({ message: 'Email not found in the database.',status:2 });
       }
     }
     catch(error){
